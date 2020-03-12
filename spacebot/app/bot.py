994316acb6cf5bac2780686a5846f90c9b9f0e8d
BlaD200 <blad600@gmail.com>
2020-03-12 20:42:09 +0200

    Rearrange files

--- spacebot/app/bot.py
+++ spacebot/app/bot.py
@@ -1,25 +1,14 @@
-import telebot
 from telebot import TeleBot
-from flask import request
 from threading import Thread
 from time import sleep
 import json
 
 from spacebot.app.find_html_components import check, find_subject_name
-from spacebot.app.main import app
 
 bot = TeleBot('937006982:AAGWKv-_o-oHJayG2oTqwJC4yjXE9puV2xE')
 
 urls_to_subjects_dict = {}
 user_threads = {}
-
-
-@app.route('/', methods=['POST'])
-def webhook():
-    json_request = request.get_json()
-    update = telebot.types.Update.de_json(json_request)
-    bot.process_new_updates([update])
-    return ''
 
 
 @bot.message_handler(content_types='text', func=lambda message: 'https://my.ukma.edu.ua/course/' in message.text)
@@ -33,9 +22,9 @@ def add_subject(message):
     else:
         urls_to_subjects_dict[message.chat.id] = []
         urls_to_subjects_dict[message.chat.id].append(message.text)
-    with open('users_data.json', 'r') as f:
+    with open('spacebot/app/users_data.json', 'r') as f:
         users_data = json.load(f)
-    with open('users_data.json', 'w') as f:
+    with open('spacebot/app/users_data.json', 'w') as f:
         chat_id = str(message.chat.id)
         if users_data.get(chat_id):
             users_data[chat_id]['subjects'].append(message.text)
@@ -53,9 +42,9 @@ def add_subject(message):
 def answer(message):
     if urls_to_subjects_dict.get(message.chat.id):
         urls_to_subjects_dict[message.chat.id].remove(message.text)
-        with open('users_data.json', 'r') as f:
+        with open('spacebot/app/users_data.json', 'r') as f:
             users_data = json.load(f)
-        with open('users_data.json', 'w') as f:
+        with open('spacebot/app/users_data.json', 'w') as f:
             chat_id = str(message.chat.id)
             if users_data.get(chat_id):
                 users_data[chat_id]['subjects'].remove(message.text)
@@ -88,9 +77,9 @@ def start_thread(chat_id, interval):
     thread.start()
     user_threads[chat_id] = thread
 
-    with open('users_data.json', 'r') as f:
+    with open('spacebot/app/users_data.json', 'r') as f:
         users_data = json.load(f)
-    with open('users_data.json', 'w') as f:
+    with open('spacebot/app/users_data.json', 'w') as f:
         chat_id = str(chat_id)
         if users_data.get(chat_id):
             users_data[chat_id]['running'] = True
@@ -104,9 +93,9 @@ def stop_look_for_free_space(message):
     if user_threads.get(chat_id):
         user_threads[chat_id].terminate()
         user_threads[chat_id].join()
-        with open('users_data.json', 'r') as f:
+        with open('spacebot/app/users_data.json', 'r') as f:
             users_data = json.load(f)
-        with open('users_data.json', 'w') as f:
+        with open('spacebot/app/users_data.json', 'w') as f:
             user_id = str(message.chat.id)
             if users_data.get(user_id):
                 users_data[user_id]['running'] = False
@@ -131,7 +120,6 @@ class LookForFreeSpaceTask(Thread):
         while self._running:
             messages = []
             for url in urls_to_subjects_dict.get(self._chat_id, []):
-                print(url)
                 response = check(url)
                 if response:
                     messages.append(f'<b>{response[1]}</b> : "<a href="{response[0]}">{find_subject_name(url)}</a>"\n')
@@ -151,7 +139,7 @@ class LookForFreeSpaceTask(Thread):
 
 
 def restore_users():
-    with open('users_data.json', 'r') as f:
+    with open('spacebot/app/users_data.json', 'r') as f:
         users_data = json.load(f)
         for user_id, user_data in users_data.items():
             user_id = int(user_id)
@@ -163,6 +151,3 @@ def restore_users():
                     urls_to_subjects_dict[user_id].append(subject_url)
             if user_data['running']:
                 start_thread(user_id, user_data['interval'])
-
-
-restore_users()
--- spacebot/app/main.py
+++ spacebot/app/main.py
@@ -1,5 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 from os import getenv
+
+from telebot.types import Update
+
+from spacebot.app.bot import restore_users, bot
 
 app = Flask(__name__)
 
@@ -9,6 +13,14 @@ def index():
     return '<h1>Telegram Bot by Vlad Synytsyn</h1>'
 
 
+@app.route('/', methods=['POST'])
+def webhook():
+    json_request = request.get_json()
+    update = Update.de_json(json_request)
+    bot.process_new_updates([update])
+    return ''
+
+
 if __name__ == '__main__':
-    context = ('cert.pem', 'key.pem')
-    app.run(host='0.0.0.0', port=getenv('PORT', 80), ssl_context=context)
+    restore_users()
+    app.run(host='0.0.0.0', port=getenv('PORT', 80))
